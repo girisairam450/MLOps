eb637ad770c532a4c4fb201a3d98e4f2e30641ad
Giri Ragam <ragam@hpe.com>
2024-02-19 08:03:01 +0530

    dbo

---
+++ csv2db.sh
@@ -0,0 +1,11 @@
+# This script extracts data from /etc/passwd file into a CSV file.
+
+# The csv data file contains the user name, user id and 
+# home directory of each user account defined in /etc/passwd
+
+# Transforms the text delimiter from ":" to ",".
+# Loads the data from the CSV file into a table in PostgreSQL database.
+
+# Extract phase
+echo "Extracting Data"
+cut -d":" -f1,3,6 sample.txt
