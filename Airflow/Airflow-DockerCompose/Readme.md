9e1e4c11f42f3ec90153445432cd7c8937a016b0
Giri Ragam <ragam@hpe.com>
2024-03-27 11:02:41 +0530

    'airflow'

---
+++ Airflow/Airflow-DockerCompose/Readme.md
@@ -0,0 +1,6 @@
+## Steps to start Airflow using docker compose
+1. Download the docker-compose file from the airflow repository `https://airflow.apache.org/docs/apache-airflow/2.8.4/docker-compose.yaml`.
+2. We need to init the environment using `docker-compose up airflow-init`.
+3. Run the airflow using `docker-compose up`.
+
+
---
+++ Airflow/my_first_dag.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner':'Giri Ragam',
+    'start_date': days_ago(0),
+    'email':['girisairam4474@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay':timedelta(minutes=5)
+}
+
+dag = DAG(
+    'my-first-dag',
+    default_args = default_args,
+    description = 'My First DAG',
+    schedule_internal = timedelta(days=1)
+)
+
+extract = BashOperator(
+    task_id = 'extract',
+    bash_commands = 'cut -d":" -f1,3,6 /etc/passwd > /home/project/airflow/dags/extracted-data.txt',
+    dag = dag,
+)
+
+transform_and_load = BashOperator(
+    task_id='transform',
+    bash_command='tr ":" "," < /home/project/airflow/dags/extracted-data.txt > /home/project/airflow/dags/transformed-data.csv',
+    dag=dag,
+)
+
+extract >> transform_and_load
